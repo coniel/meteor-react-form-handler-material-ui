751d25e9dc6f3b4049aab2b44ee36a62a652d436
Oscar Coniel <oscar@coniel.com>
2016-01-06 19:38:22 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "coniel:react-form-handler-material-ui",
   summary: "Material UI inputs for conielo:react-form-handler" ,
   git: 'https://github.com/coniel/meteor-react-form-handler-material-ui.git',
-  version: "0.3.3"
+  version: "0.3.4"
 });
 
 var MUI_VERSION = '0.14.1';
