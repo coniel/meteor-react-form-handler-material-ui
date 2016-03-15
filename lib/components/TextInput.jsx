6ed0bb582bd65406516e999b8addf217f042930d
Oscar Coniel <oscar@coniel.com>
2016-03-16 00:23:41 +0200

    Add onDoneTyping functionality to text input

--- lib/components/TextInput.jsx
+++ lib/components/TextInput.jsx
@@ -1,3 +1,5 @@
+var typingTimer;
+
 TextInput = React.createClass({
     propTypes: {
         name: React.PropTypes.string.isRequired,
@@ -7,8 +9,29 @@ TextInput = React.createClass({
             React.PropTypes.number
         ])
     },
+    getInitialState() {
+        return {
+            value: this.props.defaultValue,
+            previousValue: this.props.defaultValue
+        }
+    },
     componentWillMount() {
         FormHandler.initializeInput(this.props.formId, this.props.name, this.props.defaultValue);
+    },
+    _onDoneTyping(value) {
+        this.props.onDoneTyping(value);
+    },
+    _onKeyUp(event) {
+        if (this.props.onDoneTyping) {
+            var value = event.nativeEvent.target.value;
+            if (value !== this.state.previousValue) {
+                clearTimeout(typingTimer);
+                typingTimer = setTimeout(this._onDoneTyping.bind(this, value), this.props.doneTypingInterval || 1000);
+            }
+        }
+    },
+    _onKeyDown() {
+        clearTimeout(typingTimer);
     },
     _onChange(event) {
 
@@ -18,12 +41,27 @@ TextInput = React.createClass({
             value = Number(value);
         }
 
+        this.setState({
+            value: value,
+            previousValue: this.state.value
+        });
+
         FormHandler.inputChanged(this.props.formId, this.props.name, value);
     },
     render: function() {
         return (
             <div style={FormLayoutStyles[this.props.layoutStyle]}>
-                <MUIComponents.TextField fullWidth={true} onChange={this._onChange} {...this.props} placeholder={null} hintText={this.props.placeholder} floatingLabelText={this.props.label} errorText={this.props.errorText} />
+                <MUIComponents.TextField
+                    fullWidth={true}
+                    onChange={this._onChange}
+                    {...this.props}
+                    placeholder={null}
+                    hintText={this.props.placeholder}
+                    floatingLabelText={this.props.label}
+                    errorText={this.props.errorText}
+                    onKeyDown={this._onKeyDown}
+                    onKeyUp={this._onKeyUp}
+                />
             </div>
         )
     }
